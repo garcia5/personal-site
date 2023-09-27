5206a649b6e95fa8faa9a68aff61db2a2f41cf80
Alexander Garcia <agarcia1359@gmail.com>
2023-09-26 21:11:42 -0400

    it serves html/css

--- agarcia/__init__.py
+++ agarcia/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 def create_app():
     app = Flask(__name__)
 
+    @app.route("/")
     @app.route("/home")
     def home():
-        return "<h1>Home</h1>"
+        return render_template("home.html")
+
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
     return app
