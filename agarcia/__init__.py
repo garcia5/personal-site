6810e81891498238bc5c0df957c2deb0cd40d4b6
Alexander Garcia <agarcia1359@gmail.com>
2023-09-28 20:36:36 -0400

    add catppuccin theme, add /config page

--- agarcia/__init__.py
+++ agarcia/__init__.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from flask import Flask, render_template
 
 
@@ -9,9 +11,39 @@ def create_app():
     def home():
         return render_template("home.html")
 
-
     @app.route("/about")
     def about():
         return render_template("about.html")
+
+    @app.route("/config")
+    def config():
+        base_url = "https://github.com/garcia5/dotfiles/blob/master"
+        config_urls: List[Dict[str, str]] = [
+            {
+                "url": f"{base_url}/files/nvim/init.lua",
+                "display": "Neovim",
+            },
+            {
+                "url": f"{base_url}/files/wezterm.lua",
+                "display": "WezTerm",
+            },
+            {
+                "url": f"{base_url}/files/zshrc",
+                "display": "Zsh",
+            },
+            {
+                "url": f"{base_url}/files/functions",
+                "display": "(Mostly) FZF Integration",
+            },
+            {
+                "url": f"{base_url}/files/tmux.conf",
+                "display": "Tmux",
+            },
+            {
+                "url": f"{base_url}/files/sketchybar/sketchybarrc",
+                "display": "Sketchybar",
+            },
+        ]
+        return render_template("config.html", config_urls=config_urls)
 
     return app
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./agarcia/templates/**/*.{html,js}'],
+  content: ["./agarcia/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "mocha",
+  })],
+};
 
