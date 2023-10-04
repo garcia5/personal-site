c3e9c6900a8f40be8c332d7c191604cd80484a9b
Alexander Garcia <agarcia1359@gmail.com>
2023-10-03 23:07:00 -0400

    template 'fun' section of about page

--- agarcia/about.py
+++ agarcia/about.py
@@ -1,8 +1,10 @@
 from typing import List, Dict
+from dataclasses import dataclass
 
 from flask import Blueprint, render_template
 
 bp = Blueprint("about", __name__, url_prefix="/about")
+
 
 @bp.route("")
 def base():
@@ -18,7 +20,19 @@ def base():
             "period": "2019 - 2021",
             "position": "Software Engineer III",
             "description": "Maintain critical job scheduling infrastructure, as well as build out new product offerings for the Content & Technology Solutions department",
-        }
+        },
     ]
 
-    return render_template("about.html", work=work)
+    @dataclass
+    class Image:
+        title: str
+        filename: str
+        alt: str
+
+    fun: List[Image] = [
+        Image("Cars", "me-and-car.jpg", "Me smiling next to old porsches"),
+        Image("Climbing", "climb.jpg", "Me indoor bouldering"),
+        Image("Indy", "Indy.jpg", "Professional photo of a black dog"),
+    ]
+
+    return render_template("about.html", work=work, fun=fun)
