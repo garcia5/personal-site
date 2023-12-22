fd2d1229f127cafe3fada5a732eed11b5942e191
Alexander Garcia <agarcia1359@gmail.com>
2023-12-22 14:43:53 -0500

    updated work experience :)

--- agarcia/about.py
+++ agarcia/about.py
@@ -10,14 +10,20 @@ bp = Blueprint("about", __name__, url_prefix="/about")
 def base():
     work: List[Dict[str, str]] = [
         {
+            "company": "Capital One",
+            "period": "Jan. 2024 - Present",
+            "position": "Senior Software Engineer",
+            "description": "TBD"
+        },
+        {
             "company": "Dotdash Meredith",
-            "period": "2021 - Present",
+            "period": "Aug. 2021 - Dec. 2023",
             "position": "Senior Full Stack Developer",
             "description": "Improving various parts of the editorial process at Dotdash Meredith through work on the CMS ecosystem",
         },
         {
             "company": "FactSet Research Systems",
-            "period": "2019 - 2021",
+            "period": "Aug. 2019 - Aug. 2021",
             "position": "Software Engineer III",
             "description": "Maintain critical job scheduling infrastructure, as well as build out new product offerings for the Content & Technology Solutions department",
         },
