08be2c8beb329d5b2024b4585d2a3fc551389cd4
Alexander Garcia <agarcia1359@gmail.com>
2024-01-18 10:55:18 -0500

    add build command to generate static html

--- agarcia/__init__.py
+++ agarcia/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 
 def create_app():
@@ -15,3 +16,14 @@ def create_app():
         return render_template("home.html")
 
     return app
+
+
+def freeze():
+    app = create_app()
+    app.config['FREEZER_DESTINATION'] = "docs"
+    freezer = Freezer(app=app, with_static_files=True)
+    freezer.freeze()
+
+
+if __name__ == "__main__":
+    freeze()
