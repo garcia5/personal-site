585e119edea97f2b92e39e83175c907eed749ee2
Alexander Garcia <agarcia1359@gmail.com>
2026-01-18 21:46:02 -0500

    feat: enable hot-reloading for backend server using nodemon

--- server/start_dev.sh
+++ server/start_dev.sh
@@ -4,8 +4,8 @@ set -e
 echo "Building Docker image..."
 docker build -t alexander-personal-site-term .
 
-echo "Starting backend server..."
+echo "Starting backend server with hot-reloading..."
 # Local macOS configuration
 export SHELL_PATH="/bin/zsh"
 export DOCKER_PATH="/usr/local/bin/docker"
-node index.js
+./node_modules/.bin/nodemon index.js
